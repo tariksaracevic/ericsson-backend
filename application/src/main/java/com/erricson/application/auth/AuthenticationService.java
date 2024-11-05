9d46e4b57bda52a4139cf24d6f356bdfdc96248d
SaracevicTarik <saracevic998@gmail.com>
2024-11-05 18:53:50 +0100

    Latest changes

--- application/src/main/java/com/erricson/application/auth/AuthenticationService.java
+++ application/src/main/java/com/erricson/application/auth/AuthenticationService.java
@@ -36,6 +36,6 @@ public class AuthenticationService {
         );
         var user = userRepository.findByEmail(request.getEmail()).orElseThrow();
         var jwtToken =  jwtService.generateToken(user);
-        return AuthenticationResponse.builder().token(jwtToken).build();
+        return AuthenticationResponse.builder().token(jwtToken).email(user.getEmail()).build();
     }
 }
